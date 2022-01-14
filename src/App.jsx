fb5c859f5e61742b74954caee72a96ccc2f5848d
Jasen <60624556+jasenscode@users.noreply.github.com>
2022-01-14 11:41:15 +0000

    added test colors

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
   return (
     <div className="App">
       <HeaderLogo text="Bibliotaph" />
+      <h2>Test color</h2>
+      <h3>Test color</h3>
+      <h4>Test color</h4>
     </div>
   );
 };
