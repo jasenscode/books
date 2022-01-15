eb2c97f17a21416ca3e3d75af64fdde9ca8d5709
Jasen <60624556+jasenscode@users.noreply.github.com>
2022-01-15 22:48:00 +0000

    API fetch working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.scss";
+import { useState, useEffect } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch, faPlus } from "@fortawesome/free-solid-svg-icons";
 import HeaderLogo from "./components/HeaderLogo/HeaderLogo";
@@ -7,9 +8,22 @@ import Navbar from "./components/Navbar/Navbar";
 library.add(faSearch, faPlus);
 
 const App = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/books")
+      .then((response) => response.json())
+      .then((data) => setBooks(data));
+  }, [books]);
+
   return (
     <div className="App">
       <HeaderLogo text="Bibliotaph" />
+      <div className="one">
+        {books.map((book) => {
+          return <div>{book.name}</div>;
+        })}
+      </div>
       <Navbar />
       <h2 className="one">Test color</h2>
       <h3 className="two">Test color</h3>
