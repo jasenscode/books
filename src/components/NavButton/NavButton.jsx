f2ffd5435c16b93b84973655d6460c333acff41b
Jasen <60624556+jasenscode@users.noreply.github.com>
2022-01-16 20:15:52 +0000

    added click to navbutton rather than just icon

--- src/components/NavButton/NavButton.jsx
+++ src/components/NavButton/NavButton.jsx
@@ -6,8 +6,8 @@ const NavButton = (props) => {
   const { text, icon, handleClick } = props;
 
   return (
-    <button className="nav-btn">
-      <FontAwesomeIcon className="nav-btn__icon" icon={icon} onClick={handleClick} />
+    <button className="nav-btn" onClick={handleClick}>
+      <FontAwesomeIcon className="nav-btn__icon" icon={icon} />
       <p className="nav-btn__text">{text}</p>
     </button>
   );
