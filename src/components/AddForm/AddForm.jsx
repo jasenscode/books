850810a58e96173638a21139c62a6045f3d8681a
Jasen <60624556+jasenscode@users.noreply.github.com>
2022-01-18 09:30:55 +0000

    added dropdown for rating on add form

--- src/components/AddForm/AddForm.jsx
+++ src/components/AddForm/AddForm.jsx
@@ -3,26 +3,22 @@ import "./AddForm.scss";
 const AddForm = () => {
   return (
     <form action="" className="add-form">
-      <label htmlFor="" className="add-form__label">
-        Title
-      </label>
+      <label className="add-form__label">Title</label>
       <input type="text" className="add-form__input" />
-      <label htmlFor="" className="add-form__label">
-        Author
-      </label>
+      <label className="add-form__label">Author</label>
       <input type="text" className="add-form__input" />
-      <label htmlFor="" className="add-form__label">
-        Description
-      </label>
+      <label className="add-form__label">Description</label>
+      <textarea rows="5" cols="25" type="text" className="add-form__input" />
+      <label className="add-form__label">Image URL</label>
       <input type="text" className="add-form__input" />
-      <label htmlFor="" className="add-form__label">
-        Image URL
-      </label>
-      <input type="text" className="add-form__input" />
-      <label htmlFor="" className="add-form__label">
-        Rating
-      </label>
-      <input type="text" className="add-form__input" />
+      <label className="add-form__label">Rating</label>
+      <select className="add-form__input add-form__select" name="rating">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
       <button className="add-form__btn">Submit</button>
     </form>
   );
